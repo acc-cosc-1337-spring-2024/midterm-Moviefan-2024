4c0f24518b35ce7d18599a42c22484d1dd8bab4b
Yousif Badawy <157074622+Moviefan-2024@users.noreply.github.com>
2024-03-06 03:45:18 +0000

    Question 2 Midterm ExM

--- src/question_b/main.py
+++ src/question_b/main.py
@@ -1,1 +1,21 @@
-#add import
+def get_day_of_week(day):
+    if day == 1:
+        return "Monday"
+    elif day == 2:
+        return "Tuesday"
+    elif day == 3:
+        return "Wednesday"
+    elif day == 4:
+        return "Thursday"
+    elif day == 5:
+        return "Friday"
+    elif day == 6:
+        return "Saturday"
+    elif day == 7:
+        return "Sunday"
+    else:
+        return "Invalid number"
+
+if __name__ == "__main__":
+    day_input = int(input("Enter a number between 1 and 7: "))
+    print(get_day_of_week(day_input))
--- tests/question_tests/question_tests.py
+++ tests/question_tests/question_tests.py
@@ -1,20 +1,13 @@
-# question_tests.py
 import unittest
-from question_a import get_person_category
+from question_b import get_day_of_week
 
-class TestGetPersonCategory(unittest.TestCase):
-
-    def test_infant(self):
-        self.assertEqual(get_person_category(1), "infant")
-
-    def test_child(self):
-        self.assertEqual(get_person_category(2), "child")
-
-    def test_teenager(self):
-        self.assertEqual(get_person_category(14), "teenager")
-
-    def test_adult(self):
-        self.assertEqual(get_person_category(20), "adult")
+class TestDayOfWeek(unittest.TestCase):
+    def test_values(self):
+        self.assertEqual(get_day_of_week(0), "Invalid number")
+        self.assertEqual(get_day_of_week(1), "Monday")
+        self.assertEqual(get_day_of_week(2), "Tuesday")
+        self.assertEqual(get_day_of_week(3), "Wednesday")
+        self.assertEqual(get_day_of_week(8), "Invalid number")
 
 if __name__ == "__main__":
     unittest.main()
